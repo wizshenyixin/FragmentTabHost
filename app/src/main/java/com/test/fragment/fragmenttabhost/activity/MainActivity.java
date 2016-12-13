4c72b614a28c4225e5b79597ab3cc0b1af595d98
wizshenyixin <syx@wiz.cn>
2016-12-13 17:30:06 +0800

    显示

--- app/src/main/java/com/test/fragment/fragmenttabhost/activity/MainActivity.java
+++ app/src/main/java/com/test/fragment/fragmenttabhost/activity/MainActivity.java
@@ -1,23 +1,12 @@
 package com.test.fragment.fragmenttabhost.activity;
 
-import android.support.v4.app.FragmentTabHost;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import com.test.fragment.fragmenttabhost.R;
 
 public class MainActivity extends AppCompatActivity {
 
-    private FragmentTabHost mTabHost;
-    private String mFragmentTabs[] = {
-            "首页",
-            "提醒",
-            "工作",
-            "我的"
-    };
-    private int mImageSrcs[] = {
-            
-    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
